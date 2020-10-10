029969263570c2e81a5edb8198ef10d6eee62456
nine-eleven <thedevilwearspradaoops@gmail.com>
2020-10-10 14:11:18 +0300

    feat: add dream-team & hanoi-tower & what-season

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,19 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) {return false};
+  let newArray = [];
+  members.forEach( 
+    (item) => {if (typeof item === "string") { newArray.push(item.toUpperCase()) } }
+   );
+
+  let arrayEnd = newArray.map ( (item) => {
+   return item.match(/\S/g);
+  } );
+
+  arrayEnd.sort();
+
+
+  return  arrayEnd.reduce( (sum, item) => sum + item[0], "" );
+
 };

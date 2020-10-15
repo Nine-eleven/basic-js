1d2a2544cc06514aa67f9c16e8e8c28af6da9a3f
nine-eleven <thedevilwearspradaoops@gmail.com>
2020-10-15 23:20:55 +0300

    feat: add Vigenere-cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,69 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor (valueBoolean) {
+    this.valueBoolean = valueBoolean === false;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
   }
-}
+
+
+
+
+  encrypt (message, key) {
+    this.checkValue(message, key);
+    return this.getResult(message, key);
+  }
+
+
+
+  decrypt (message, key) {
+    this.checkValue(message, key);
+    return this.getResult(message, key, true);
+  }
+
+
+
+  checkValue (message, key) {
+
+    if (typeof message === "undefined" || typeof key === "undefined") {throw new Error()};
+
+  }
+
+
+
+  getResult (message, key, isDecrypt = false) {
+    let result = '';
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      if (typeof key[j] === "undefined") j = 0;
+
+      if (!this.alphabet.includes(message[i])) {
+        result += message[i];
+        continue;
+      }
+
+      let keyIndex = this.alphabet.indexOf(key[j]);
+      let keyRow = this.alphabet.slice(keyIndex, this.alphabet.length).concat(this.alphabet.slice(0, keyIndex));
+      
+      let messageIndex = (isDecrypt ? keyRow : this.alphabet).indexOf(message[i]);
+
+      result += (isDecrypt ? this.alphabet : keyRow)[messageIndex];
+      j++;
+    }
+
+
+
+    return this.valueBoolean ? result.split('').reverse().join('') : result;
+
+
+  }
+}  
+
+
+
 
 module.exports = VigenereCipheringMachine;
